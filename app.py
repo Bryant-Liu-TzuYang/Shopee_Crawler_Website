54e52234a9ee57a48731c8e11dc6ab92d76f3141
Bryant Liu <bryant98360410@gmail.com>
2025-02-04 21:11:01 +0800

    update

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
+import pyodbc, struct
+from azure import identity
+from typing import Union
+from pydantic import BaseModel
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
+
+connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 
 app = Flask(__name__)
 
@@ -21,5 +27,17 @@ def index():
 
 
 
+def get_conn():
+    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
+    conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+    return conn
+
+
 if __name__ == '__main__':
    app.run()
+
+
+
