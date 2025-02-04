95522f8e339de0946afdc0ce1f49d6d297b8c9e2
Bryant Liu <bryant98360410@gmail.com>
2025-02-04 20:48:15 +0800

    update

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-from db import get_db
 
 app = Flask(__name__)
 
@@ -15,21 +14,11 @@ def index():
     OUTPUT_FOLDER = 'outputs'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
-    files = list()
-    db = get_db()
 
-    db.execute(
-        "INSERT INTO files (filename, status, stamp) VALUES (?, ?, ?)",
-        ("aFilename", 'processing', "time_stamp")
-    )
+    files = list()
 
     return render_template('home_page.html', files=files)
 
-
-
-
-import db
-db.init_app(app)
 
 
 if __name__ == '__main__':
